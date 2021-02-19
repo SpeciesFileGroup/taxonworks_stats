ce8f50d64489aca588c3ee4f5095d35e630672b5
mjy <diapriid@gmail.com>
2021-02-19 13:31:39 -0600

    whitespace on what is this

--- src/components/WhatThis.vue
+++ src/components/WhatThis.vue
@@ -10,15 +10,17 @@
     <modal-component
       v-if="showModal"
       @close="setModalView(false)">
-      <h3 slot="header">What's this</h3>
+      <h3 slot="header">What is this?</h3>
       <div slot="body">
-        <p>The purpose of this website is to provide a quick glimpse into the scope of TaxonWorks use.</p>
+        <p>The purpose of this website is to provide a quick glimpse into the scope of TaxonWorks' use.</p>
+        <p>
         <ul>
-          <li>There is no one TaxonWorks, the numbers here reflect a single instance, as selected on the top of the page.</li>
+          <li>There is no one TaxonWorks, the numbers here reflect a single instance, the one selected at the top.</li>
           <li>Currently, clicking on a card takes you to the technical definition of that concept, as defined in code.  We'll work to make this more accessible in the future.</li>
-          <li>Find out more about the project and code via the links on top.</li>
+          <li>Find out more about the project and code via links at the top right.</li>
         </ul>
-        <p>This website is also an <a href="https://github.com/SpeciesFileGroup/taxonworks_stats">open-source demonstration</a> of how to build applications off of a TaxonWorks API, and serve them freely.</p>
+        </p>
+        <p>This website is also an <a href="https://github.com/SpeciesFileGroup/taxonworks_stats">open-source demonstration</a> of how to build applications on top of a TaxonWorks API, and how to serve those application freely.</p>
         <p>Questions? We're always <a href="https://gitter.im/SpeciesFileGroup/taxonworks">available to chat</a>.</p>
       </div>
     </modal-component>
@@ -46,9 +48,15 @@ export default {
 }
 </script>
 <style>
+  .modal-body {
+    line-height: 1.5;
+  }
   span.modal-button {
     white-space: nowrap;
     cursor: pointer;
     color: #00845D;
+  }
+  li {
+    padding: 0.4em;
   }
 </style>
