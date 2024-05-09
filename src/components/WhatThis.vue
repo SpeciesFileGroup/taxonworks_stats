77029350b871dfb31aedfc43271a84ebe5855d49
jlpereira <pepelui@outlook.com>
2024-05-09 13:24:55 -0300

    Fix modal

--- src/components/WhatThis.vue
+++ src/components/WhatThis.vue
@@ -7,33 +7,38 @@
       @click="setModalView(true)">
       What is this?
     </a>
-    <modal-component
+    <VModal
       v-if="showModal"
-      @close="setModalView(false)">
-      <h3 slot="header">What is this?</h3>
-      <div slot="body">
-        <p>The purpose of this website is to provide a quick glimpse into the scope of TaxonWorks' use.</p>
-        <p>
-        <ul>
-          <li>There is no one TaxonWorks, the numbers here reflect a single instance, the one selected at the top.</li>
-          <li>Currently, clicking on a card takes you to the technical definition of that concept, as defined in code.  We'll work to make this more accessible in the future.</li>
-          <li>Find out more about the project and code via links at the top right.</li>
-        </ul>
-        </p>
-        <p>This website is also an <a href="https://github.com/SpeciesFileGroup/taxonworks_stats">open-source demonstration</a> of how to build applications on top of a TaxonWorks API, and how to serve those application freely.</p>
-        <p>Questions? We're always <a href="https://gitter.im/SpeciesFileGroup/taxonworks">available to chat</a>.</p>
-      </div>
-    </modal-component>
+      @close="setModalView(false)"
+    >
+      <template #header>
+        <h3>What is this?</h3>
+      </template>
+      <template #body>
+        <div>
+          <p>The purpose of this website is to provide a quick glimpse into the scope of TaxonWorks' use.</p>
+          <p>
+          <ul>
+            <li>There is no one TaxonWorks, the numbers here reflect a single instance, the one selected at the top.</li>
+            <li>Currently, clicking on a card takes you to the technical definition of that concept, as defined in code.  We'll work to make this more accessible in the future.</li>
+            <li>Find out more about the project and code via links at the top right.</li>
+          </ul>
+          </p>
+          <p>This website is also an <a href="https://github.com/SpeciesFileGroup/taxonworks_stats">open-source demonstration</a> of how to build applications on top of a TaxonWorks API, and how to serve those application freely.</p>
+          <p>Questions? We're always <a href="https://gitter.im/SpeciesFileGroup/taxonworks">available to chat</a>.</p>
+        </div>
+      </template>
+    </VModal>
   </div>
 </template>
 
 <script>
 
-import ModalComponent from '@/components/Modal.vue'
+import VModal from '@/components/Modal.vue'
 
 export default {
   components: {
-    ModalComponent
+    VModal
   },
   data () {
     return {
